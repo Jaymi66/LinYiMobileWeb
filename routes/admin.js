d7cc8dbaba48737a5484d5c62892f77264760121
晓强 <878773559@qq.com>
2016-05-31 16:46:04 +0800

    测试

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,19 @@ var express = require('express')
 var router = express.Router()
 
 // admin page
-router.get('/', function(request, response){
-	response.render('admin/login', {
+router.get('/', function(req, res){
+	res.render('admin/login', {
 		title: 'Kinms后台管理'
 	})
 })
 
-router.get('/index', function(request, response){
-	response.render('admin/index', {
+router.get('/login:username&:password', function(req, res) {
+	console.log(req.params.username)
+	res.render('admin/index')
+})
+
+router.get('/index', function(req, res){
+	res.render('admin/index', {
 		title: 'Kinms后台管理'
 	})
 })
