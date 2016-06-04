20307cd53c26ba6add5992d63bafd971ec83f304
晓强 <878773559@qq.com>
2016-06-04 11:15:53 +0800

    123

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+var Zone = require('../schemas/zone')
+
 // home page
 router.get('/', function(request, response){
-	response.render('index', {
-		title: "Kinms首页"
-	})
+	Zone.findAll({}).then(function(project){
+		// console.log(project)
+		response.render('index', {
+			title: 'Kinms后台管理',
+			zones: project
+		})
+		return;
+	});
 })
 
 router.get('/index', function(request, response){
-	response.render('index', {
-		title: "首页"
-	})
+
+	Zone.findAll({}).then(function(project){
+		// console.log(project)
+		response.render('index', {
+			title: 'Kinms后台管理',
+			zones: project
+		})
+		return;
+	});
+
+	// response.render('index', {
+	// 	title: "首页"
+	// })
 })
 
 router.get('/detail', function(request, response){
+
 	response.render('detail', {
 		title: "Kinms首页"
 	})
